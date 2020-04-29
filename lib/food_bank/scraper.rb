951e3b06f211293a45e3f02766376eedb3cd843d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-29 21:59:57 +0000

    More notes on video

--- lib/food_bank/scraper.rb
+++ lib/food_bank/scraper.rb
@@ -3,8 +3,12 @@
 #
 #00:09:56 if you simply want to see if nokogiri is getting data (HTML in her case)
 # site = "https://www.xyz.com"
-# page = Nokogiri::HTML(open(site))
-# puts page
+# doc = Nokogiri::HTML(open(site))
+# puts doc
+#
+#00:15:05
+#The top level that contains all the food banks MAY be doc.css(".Folder")
+#Getting all the locations MAY be doc.css(".Folder.Placemark")
 
 require 'nokogiri'
 require 'open-uri' #is this needed for an XML file?
