078c31e3d16150ed90784ed4209bfb45530765ad
Elliot Blanchard <public@elliotblanchard.com>
2020-05-01 22:20:46 +0000

    First pass at data hash

--- lib/food_bank/scraper.rb
+++ lib/food_bank/scraper.rb
@@ -5,16 +5,58 @@ class FoodBank::Scraper
   #http://ruby.bastardsbook.com/chapters/html-parsing/
   
   #Array of all the food banks: doc.css("Placemark")
-  #Array of all the Food Bank names: doc.css("Placemark name") / doc.css("Placemark name").text
-  #Array of all the Food Bank addresses: doc.css("Placemark address") / doc.css("Placemark address").text
-  #...contact name: ????? doc.css("Placemark ExtendedData Data[name='Contact']").text
-  #...phone: ????? doc.css("Placemark ExtendedData Data[name='Phone']").text
+  #Array of all the Food Bank names: doc.css("Placemark name") / doc.css("Placemark name").text / bank.css("name").text
+  #Array of all the Food Bank addresses: doc.css("Placemark address") / doc.css("Placemark address").text / bank.css("address").text
+  #...contact name: doc.css("Placemark ExtendedData Data[name='Contact']").text / bank.css("ExtendedData Data[name='Contact']").text
+  #...phone: doc.css("Placemark ExtendedData Data[name='Phone']").text / bank.css("ExtendedData Data[name='Phone']").text - needs strip
+  #...common values in program type: "Pantry"
+  #...program type: bank.css("ExtendedData Data[name='Program Type']").text.strip
+  #...address: bank.css("ExtendedData Data[name='ç']").text.strip
+  #...city (borough): bank.css("ExtendedData Data[name='City']").text.strip
+  #...state: bank.css("ExtendedData Data[name='State']").text.strip
+  #...zip: bank.css("ExtendedData Data[name='ZIP Code']").text.strip
+  #...common values in days: "-" "closed" "8:00 AM - 10:00 AM" "1:00 PM - 3:00 PM (By appointment only)"
+  #...monday: bank.css("ExtendedData Data[name='Monday']").text.strip
+  #...tuesday: bank.css("ExtendedData Data[name='Tuesday']").text.strip
+  #...wednesday: bank.css("ExtendedData Data[name='Wednesday']").text.strip
+  #...thursday: bank.css("ExtendedData Data[name='Thursday']").text.strip
+  #...friday: bank.css("ExtendedData Data[name='Friday']").text.strip
+  #...saturday: bank.css("ExtendedData Data[name='Saturday']").text.strip
+  #...sunday: bank.css("ExtendedData Data[name='Sunday']").text.strip
   #page.css("li[data-category='news']")
   
   def self.scrape_banks
     xml = File.read('fixtures/Food_Bank_For_NYC_Open_Members_as_of_42820.kml')
-    doc = Nokogiri::XML(xml)    
+    doc = Nokogiri::XML(xml)  
+    banks = []
+    
+    doc.css("Placemark").each_with_index do |bank, index|
+      days = []
+      days[0] = bank.css("ExtendedData Data[name='Sunday']").text.strip
+      days[1] = bank.css("ExtendedData Data[name='Monday']").text.strip
+      days[2] = bank.css("ExtendedData Data[name='Tuesday']").text.strip
+      days[3] = bank.css("ExtendedData Data[name='Wednesday']").text.strip
+      days[4] = bank.css("ExtendedData Data[name='Thursday']").text.strip
+      days[5] = bank.css("ExtendedData Data[name='Friday']").text.strip
+      days[6] = bank.css("ExtendedData Data[name='Saturday']").text.strip
+      
+      banks[index] = {
+        :name => bank.css("name").text.strip
+        :address => bank.css("address").text.strip
+        :contact => bank.css("ExtendedData Data[name='Contact']").text.strip
+        :phone => bank.css("ExtendedData Data[name='Phone']").text.strip
+        :program => bank.css("ExtendedData Data[name='Program Type']").text.strip
+        :address => bank.css("ExtendedData Data[name='Address']").text.strip
+        :city => bank.css("ExtendedData Data[name='City']").text.strip
+        :state => bank.css("ExtendedData Data[name='State']").text.strip
+        :zip => bank.css("ExtendedData Data[name='ZIP Code']").text.strip
+        :days => days
+      }
+      
+    end
+    
     binding.pry
+    
   end
   
 end
