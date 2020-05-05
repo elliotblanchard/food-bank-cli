2f14a3733bd0abd499112de7c7a09f1c4a3ce6f0
Elliot Blanchard <public@elliotblanchard.com>
2020-05-05 02:53:04 +0000

    More comment cleanup

--- lib/food_bank/scraper.rb
+++ lib/food_bank/scraper.rb
@@ -1,30 +1,5 @@
 class FoodBank::Scraper
   
-  #Tutorial on searching XML with Nokogiri
-  #https://nokogiri.org/tutorials/searching_a_xml_html_document.
-  #http://ruby.bastardsbook.com/chapters/html-parsing/
-  
-  #Array of all the food banks: doc.css("Placemark")
-  #Array of all the Food Bank names: doc.css("Placemark name") / doc.css("Placemark name").text / bank.css("name").text
-  #Array of all the Food Bank addresses: doc.css("Placemark address") / doc.css("Placemark address").text / bank.css("address").text
-  #...contact name: doc.css("Placemark ExtendedData Data[name='Contact']").text / bank.css("ExtendedData Data[name='Contact']").text
-  #...phone: doc.css("Placemark ExtendedData Data[name='Phone']").text / bank.css("ExtendedData Data[name='Phone']").text - needs strip
-  #...common values in program type: "Pantry"
-  #...program type: bank.css("ExtendedData Data[name='Program Type']").text.strip
-  #...address: bank.css("ExtendedData Data[name='ç']").text.strip
-  #...city (borough): bank.css("ExtendedData Data[name='City']").text.strip
-  #...state: bank.css("ExtendedData Data[name='State']").text.strip
-  #...zip: bank.css("ExtendedData Data[name='ZIP Code']").text.strip
-  #...common values in days: "-" "closed" "8:00 AM - 10:00 AM" "1:00 PM - 3:00 PM (By appointment only)"
-  #...monday: bank.css("ExtendedData Data[name='Monday']").text.strip
-  #...tuesday: bank.css("ExtendedData Data[name='Tuesday']").text.strip
-  #...wednesday: bank.css("ExtendedData Data[name='Wednesday']").text.strip
-  #...thursday: bank.css("ExtendedData Data[name='Thursday']").text.strip
-  #...friday: bank.css("ExtendedData Data[name='Friday']").text.strip
-  #...saturday: bank.css("ExtendedData Data[name='Saturday']").text.strip
-  #...sunday: bank.css("ExtendedData Data[name='Sunday']").text.strip
-  #page.css("li[data-category='news']")
-  
   def self.scrape_banks(path)
     xml = File.read(path)
     doc = Nokogiri::XML(xml)  
